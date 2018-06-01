01d11ff8c9e1142acd57f67153e4c33ce54959e9
徐大賢 <garyhsu1209@gmail.com>
2018-06-01 16:39:07 +0800

    home paga added

--- gary/urls.py
+++ gary/urls.py
@@ -4,4 +4,6 @@ from . import views
 app_name = 'gary'
 urlpatterns = [
     path('',views.index,name = 'index'),
+    path('login/', views.login,name='login'),
+    path('comments',views.comments,name = 'comments'),
 ]
--- gary/views.py
+++ gary/views.py
@@ -1,7 +1,50 @@
-from django.shortcuts import render
+import requests
+
+from django.shortcuts import render, redirect
+from django.conf import settings
+from django.contrib import messages
+
+from .models import Comment
+from .forms import CommentForm
 
 # Create your views here.
 
 def index(request):
 	# return HttpResponse("<h2>Home about</h2>")
 	return render(request,'gary/index.html')
+
+
+
+
+def comments(request):
+    comments_list = Comment.objects.order_by('-created_at')
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+
+            ''' Begin reCAPTCHA validation '''
+            recaptcha_response = request.POST.get('g-recaptcha-response')
+            data = {
+                'secret': settings.GOOGLE_RECAPTCHA_SECRET_KEY,
+                'response': recaptcha_response
+            }
+            r = requests.post('https://www.google.com/recaptcha/api/siteverify', data=data)
+            result = r.json()
+            ''' End reCAPTCHA validation '''
+
+            if result['success']:
+                form.save()
+                messages.success(request, 'New comment added with success!')
+            else:
+                messages.error(request, 'Invalid reCAPTCHA. Please try again.')
+
+            return redirect('gary/comments')
+    else:
+        form = CommentForm()
+
+    return render(request, 'gary/comments.html', {'comments': comments_list, 'form': form})
+
+
+def login(request):
+    return render(request,'gary/login.html')
