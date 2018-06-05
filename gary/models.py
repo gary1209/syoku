f5b53bdc7b1c4fcfef069c0e2ca32161e44b2211
徐大賢 <garyhsu1209@gmail.com>
2018-06-05 14:37:26 +0800

    fisrt version done

--- gary/models.py
+++ gary/models.py
@@ -14,3 +14,30 @@ class Comment(models.Model):
     class Meta:
         verbose_name = 'comment'
         verbose_name_plural = 'comments'
+
+class Plan(models.Model):
+    useremail = models.EmailField(max_length=100,blank=False)
+    title = models.CharField(max_length=50,null=True)
+    mon1 = models.CharField(max_length=50,null=True)
+    mon2 = models.CharField(max_length=50,null=True)
+    mon3 = models.CharField(max_length=50,null=True)
+    tue1 = models.CharField(max_length=50,null=True)
+    tue2 = models.CharField(max_length=50,null=True)
+    tue3 = models.CharField(max_length=50,null=True)
+    wed1 = models.CharField(max_length=50,null=True)
+    wed2 = models.CharField(max_length=50,null=True)
+    wed3 = models.CharField(max_length=50,null=True)
+    thu1 = models.CharField(max_length=50,null=True)
+    thu2 = models.CharField(max_length=50,null=True)
+    thu3 = models.CharField(max_length=50,null=True)
+    fri1 = models.CharField(max_length=50,null=True)
+    fri2 = models.CharField(max_length=50,null=True)
+    fri3 = models.CharField(max_length=50,null=True)
+    sat1 = models.CharField(max_length=50,null=True)
+    sat2 = models.CharField(max_length=50,null=True)
+    sat3 = models.CharField(max_length=50,null=True)
+    sun1 = models.CharField(max_length=50,null=True)
+    sun2 = models.CharField(max_length=50,null=True)
+    sun3 = models.CharField(max_length=50,null=True)
+    class Meta:
+        db_table = "plan"
