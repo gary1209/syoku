0beb94d2b6ebee53f8076d9abc8ed4cb994703d9
ghostyy <ghostyy@gmail.com>
2018-06-05 20:30:59 +0800

    06/05
    garyyyyyyyyyyyyyyyyyyyyyy

--- gary/views.py
+++ gary/views.py
@@ -95,3 +95,473 @@ def comments(request):
 
 def login(request):
     return render(request, 'gary/login.html')
+
+
+def history(request):
+    
+    history = Plan.objects.all()
+
+    return render(request, 'gary/history.html',locals())
+
+
+def historydata(request,id):
+    
+    history = Plan.objects.filter(id=int(id))
+    
+    
+    for historys in history:
+        if historys.mon1:
+            a=historys.mon1.split(']')[1]
+            b=historys.mon1.split(']')[0]
+            c=b.split('[')[1]
+            print(c)
+            print(a)
+            if c == "店家":
+                storemenus = Storemenu.objects.all()
+                for storemenuss in storemenus:
+                    if a == storemenuss.cname:
+                        print(storemenuss.cimg)
+
+                        img_gary_mon1 = storemenuss.cimg
+                        
+            elif c == "食譜":
+                recipes = Recipe.objects.all()
+                for recipess in recipes:
+                    if a == recipess.recname:
+                        print(recipess.reccover)
+
+                        img_gary_mon1 = recipess.reccover
+        else:
+            img_gary_mon1 = "blank.jpg"
+    for historys in history:
+        if historys.mon2:
+            a=historys.mon2.split(']')[1]
+            b=historys.mon2.split(']')[0]
+            c=b.split('[')[1]
+            print(c)
+            print(a)
+            if c == "店家":
+                storemenus = Storemenu.objects.all()
+                for storemenuss in storemenus:
+                    if a == storemenuss.cname:
+                        print(storemenuss.cimg)
+                        img_gary_mon2 = storemenuss.cimg
+                        
+            elif c == "食譜":
+                recipes = Recipe.objects.all()
+                for recipess in recipes:
+                    if a == recipess.recname:
+                        print(recipess.reccover)
+                        img_gary_mon2 = recipess.reccover
+        else:
+            img_gary_mon2 = "blank.jpg"
+            
+    for historys in history:
+        if historys.mon3:
+            a=historys.mon3.split(']')[1]
+            b=historys.mon3.split(']')[0]
+            c=b.split('[')[1]
+            print(c)
+            print(a)
+            if c == "店家":
+                storemenus = Storemenu.objects.all()
+                for storemenuss in storemenus:
+                    if a == storemenuss.cname:
+                        print(storemenuss.cimg)
+                        img_gary_mon3 = storemenuss.cimg
+            elif c == "食譜":
+                recipes = Recipe.objects.all()
+                for recipess in recipes:
+                    if a == recipess.recname:
+                        print(recipess.reccover)
+                        img_gary_mon3 = recipess.reccover
+        else:
+            img_gary_mon3 = "blank.jpg"
+            
+    for historys in history:
+        if historys.tue1:
+            a=historys.tue1.split(']')[1]
+            b=historys.tue1.split(']')[0]
+            c=b.split('[')[1]
+            print(c)
+            print(a)
+            if c == "店家":
+                storemenus = Storemenu.objects.all()
+                for storemenuss in storemenus:
+                    if a == storemenuss.cname:
+                        print(storemenuss.cimg)
+                        img_gary_tue1 = storemenuss.cimg
+            elif c == "食譜":
+                recipes = Recipe.objects.all()
+                for recipess in recipes:
+                    if a == recipess.recname:
+                        print(recipess.reccover)
+                        img_gary_tue1 = recipess.reccover
+        else:
+            
+            img_gary_tue1 = "blank.jpg"
+    for historys in history:
+        if historys.tue2:
+            a=historys.tue2.split(']')[1]
+            b=historys.tue2.split(']')[0]
+            c=b.split('[')[1]
+            print(c)
+            print(a)
+            if c == "店家":
+                storemenus = Storemenu.objects.all()
+                for storemenuss in storemenus:
+                    if a == storemenuss.cname:
+                        print(storemenuss.cimg)
+                        img_gary_tue2 = storemenuss.cimg
+            elif c == "食譜":
+                recipes = Recipe.objects.all()
+                for recipess in recipes:
+                    if a == recipess.recname:
+                        print(recipess.reccover)
+                        img_gary_tue2 = recipess.reccover
+
+        else:
+            
+            img_gary_tue2 = "blank.jpg"
+    for historys in history:
+        if historys.tue3:
+            a=historys.tue3.split(']')[1]
+            b=historys.tue3.split(']')[0]
+            c=b.split('[')[1]
+            print(c)
+            print(a)
+            if c == "店家":
+                storemenus = Storemenu.objects.all()
+                for storemenuss in storemenus:
+                    if a == storemenuss.cname:
+                        print(storemenuss.cimg)
+                        img_gary_tue3 = storemenuss.cimg
+            elif c == "食譜":
+                recipes = Recipe.objects.all()
+                for recipess in recipes:
+                    if a == recipess.recname:
+                        print(recipess.reccover)
+                        img_gary_tue3 = recipess.reccover
+        else:
+            img_gary_tue3 = "blank.jpg"
+    for historys in history:
+        if historys.wed1:
+            a=historys.wed1.split(']')[1]
+            b=historys.wed1.split(']')[0]
+            c=b.split('[')[1]
+            print(c)
+            print(a)
+            if c == "店家":
+                storemenus = Storemenu.objects.all()
+                for storemenuss in storemenus:
+                    if a == storemenuss.cname:
+                        print(storemenuss.cimg)
+                        img_gary_wed1 = storemenuss.cimg
+
+            elif c == "食譜":
+                recipes = Recipe.objects.all()
+                for recipess in recipes:
+                    if a == recipess.recname:
+                        print(recipess.reccover)
+                        img_gary_wed1 = recipess.reccover
+        else:
+            img_gary_wed1 = "blank.jpg"
+    for historys in history:
+        if historys.wed2:
+            a=historys.wed2.split(']')[1]
+            b=historys.wed2.split(']')[0]
+            c=b.split('[')[1]
+            print(c)
+            print(a)
+            if c == "店家":
+                storemenus = Storemenu.objects.all()
+                for storemenuss in storemenus:
+                    if a == storemenuss.cname:
+                        print(storemenuss.cimg)
+                        img_gary_wed2 = storemenuss.cimg
+            elif c == "食譜":
+                recipes = Recipe.objects.all()
+                for recipess in recipes:
+                    if a == recipess.recname:
+                        print(recipess.reccover)
+                        img_gary_wed2 = recipess.reccover
+        else:
+            img_gary_wed2 = "blank.jpg"
+    for historys in history:
+        if historys.wed3:
+            a=historys.wed3.split(']')[1]
+            b=historys.wed3.split(']')[0]
+            c=b.split('[')[1]
+            print(c)
+            print(a)
+            if c == "店家":
+                storemenus = Storemenu.objects.all()
+                for storemenuss in storemenus:
+                    if a == storemenuss.cname:
+                        print(storemenuss.cimg)
+                        img_gary_wed3 = storemenuss.cimg
+            elif c == "食譜":
+                recipes = Recipe.objects.all()
+                for recipess in recipes:
+                    if a == recipess.recname:
+                        print(recipess.reccover)
+                        img_gary_wed3 = recipess.reccover
+        else:
+            img_gary_wed3 = "blank.jpg"
+    for historys in history:
+        if historys.thu1:
+            a=historys.thu1.split(']')[1]
+            b=historys.thu1.split(']')[0]
+            c=b.split('[')[1]
+            print(c)
+            print(a)
+            if c == "店家":
+                storemenus = Storemenu.objects.all()
+                for storemenuss in storemenus:
+                    if a == storemenuss.cname:
+                        print(storemenuss.cimg)
+                        img_gary_thu1 = storemenuss.cimg
+            elif c == "食譜":
+                recipes = Recipe.objects.all()
+                for recipess in recipes:
+                    if a == recipess.recname:
+                        print(recipess.reccover)
+                        img_gary_thu1 = recipess.reccover
+        else:
+            img_gary_thu1 = "blank.jpg"
+    for historys in history:
+        if historys.thu2:
+            a=historys.thu2.split(']')[1]
+            b=historys.thu2.split(']')[0]
+            c=b.split('[')[1]
+            print(c)
+            print(a)
+            if c == "店家":
+                storemenus = Storemenu.objects.all()
+                for storemenuss in storemenus:
+                    if a == storemenuss.cname:
+                        print(storemenuss.cimg)
+                        img_gary_thu2 = storemenuss.cimg
+            elif c == "食譜":
+                recipes = Recipe.objects.all()
+                for recipess in recipes:
+                    if a == recipess.recname:
+                        print(recipess.reccover)
+                        img_gary_thu2 = recipess.reccover
+        else:
+            img_gary_thu2 = "blank.jpg"
+    for historys in history:
+        if historys.thu3:
+            a=historys.thu3.split(']')[1]
+            b=historys.thu3.split(']')[0]
+            c=b.split('[')[1]
+            print(c)
+            print(a)
+            if c == "店家":
+                storemenus = Storemenu.objects.all()
+                for storemenuss in storemenus:
+                    if a == storemenuss.cname:
+                        print(storemenuss.cimg)
+                        img_gary_thu3 = storemenuss.cimg
+            elif c == "食譜":
+                recipes = Recipe.objects.all()
+                for recipess in recipes:
+                    if a == recipess.recname:
+                        print(recipess.reccover)
+                        img_gary_thu3 = recipess.reccover
+        else:
+            img_gary_thu3 = "blank.jpg"
+    for historys in history:
+        if historys.fri1:
+            a=historys.fri1.split(']')[1]
+            b=historys.fri1.split(']')[0]
+            c=b.split('[')[1]
+            print(c)
+            print(a)
+            if c == "店家":
+                storemenus = Storemenu.objects.all()
+                for storemenuss in storemenus:
+                    if a == storemenuss.cname:
+                        print(storemenuss.cimg)
+                        img_gary_fri1 = storemenuss.cimg
+            elif c == "食譜":
+                recipes = Recipe.objects.all()
+                for recipess in recipes:
+                    if a == recipess.recname:
+                        print(recipess.reccover)
+                        img_gary_fri1 = recipess.reccover
+        else:
+            img_gary_fri1 = "blank.jpg"
+    for historys in history:
+        if historys.fri2:
+            a=historys.fri2.split(']')[1]
+            b=historys.fri2.split(']')[0]
+            c=b.split('[')[1]
+            print(c)
+            print(a)
+            if c == "店家":
+                storemenus = Storemenu.objects.all()
+                for storemenuss in storemenus:
+                    if a == storemenuss.cname:
+                        print(storemenuss.cimg)
+                        img_gary_fri2 = storemenuss.cimg
+            elif c == "食譜":
+                recipes = Recipe.objects.all()
+                for recipess in recipes:
+                    if a == recipess.recname:
+                        print(recipess.reccover)
+                        img_gary_fri2 = recipess.reccover
+        else:
+            img_gary_fri2 = "blank.jpg"
+    for historys in history:
+        if historys.fri3:
+            a=historys.fri3.split(']')[1]
+            b=historys.fri3.split(']')[0]
+            c=b.split('[')[1]
+            print(c)
+            print(a)
+            if c == "店家":
+                storemenus = Storemenu.objects.all()
+                for storemenuss in storemenus:
+                    if a == storemenuss.cname:
+                        print(storemenuss.cimg)
+                        img_gary_fri3 = storemenuss.cimg
+            elif c == "食譜":
+                recipes = Recipe.objects.all()
+                for recipess in recipes:
+                    if a == recipess.recname:
+                        print(recipess.reccover)
+                        img_gary_fri3 = recipess.reccover
+        else:
+            img_gary_fri3 = "blank.jpg"
+    for historys in history:
+        if historys.sat1:
+            a=historys.sat1.split(']')[1]
+            b=historys.sat1.split(']')[0]
+            c=b.split('[')[1]
+            print(c)
+            print(a)
+            if c == "店家":
+                storemenus = Storemenu.objects.all()
+                for storemenuss in storemenus:
+                    if a == storemenuss.cname:
+                        print(storemenuss.cimg)
+                        img_gary_sat1 = storemenuss.cimg
+            elif c == "食譜":
+                recipes = Recipe.objects.all()
+                for recipess in recipes:
+                    if a == recipess.recname:
+                        print(recipess.reccover)
+                        img_gary_sat1 = recipess.reccover
+        else:
+            img_gary_sat1 = "blank.jpg"
+    for historys in history:
+        if historys.sat2:
+            a=historys.sat2.split(']')[1]
+            b=historys.sat2.split(']')[0]
+            c=b.split('[')[1]
+            print(c)
+            print(a)
+            if c == "店家":
+                storemenus = Storemenu.objects.all()
+                for storemenuss in storemenus:
+                    if a == storemenuss.cname:
+                        print(storemenuss.cimg)
+                        img_gary_sat2 = storemenuss.cimg
+            elif c == "食譜":
+                recipes = Recipe.objects.all()
+                for recipess in recipes:
+                    if a == recipess.recname:
+                        print(recipess.reccover)
+                        img_gary_sat2 = recipess.reccover
+        else:
+            img_gary_sat2 = "blank.jpg"
+    for historys in history:
+        if historys.sat3:
+            a=historys.sat3.split(']')[1]
+            b=historys.sat3.split(']')[0]
+            c=b.split('[')[1]
+            print(c)
+            print(a)
+            if c == "店家":
+                storemenus = Storemenu.objects.all()
+                for storemenuss in storemenus:
+                    if a == storemenuss.cname:
+                        print(storemenuss.cimg)
+                        img_gary_sat3 = storemenuss.cimg
+            elif c == "食譜":
+                recipes = Recipe.objects.all()
+                for recipess in recipes:
+                    if a == recipess.recname:
+                        print(recipess.reccover)
+                        img_gary_sat3 = recipess.reccover
+        else:
+            img_gary_sat3 = "blank.jpg"
+
+    for historys in history:
+        if historys.sun1:
+            a=historys.sun1.split(']')[1]
+            b=historys.sun1.split(']')[0]
+            c=b.split('[')[1]
+            print(c)
+            print(a)
+            if c == "店家":
+                storemenus = Storemenu.objects.all()
+                for storemenuss in storemenus:
+                    if a == storemenuss.cname:
+                        print(storemenuss.cimg)
+                        img_gary_sun1 = storemenuss.cimg
+            elif c == "食譜":
+                recipes = Recipe.objects.all()
+                for recipess in recipes:
+                    if a == recipess.recname:
+                        print(recipess.reccover)
+                        img_gary_sun1 = recipess.reccover
+        else:
+            img_gary_sun1 = "blank.jpg"
+    for historys in history:
+        if historys.sun2:
+            a=historys.sun2.split(']')[1]
+            b=historys.sun2.split(']')[0]
+            c=b.split('[')[1]
+            print(c)
+            print(a)
+            if c == "店家":
+                storemenus = Storemenu.objects.all()
+                for storemenuss in storemenus:
+                    if a == storemenuss.cname:
+                        print(storemenuss.cimg)
+                        img_gary_sun2 = storemenuss.cimg
+            elif c == "食譜":
+                recipes = Recipe.objects.all()
+                for recipess in recipes:
+                    if a == recipess.recname:
+                        print(recipess.reccover)
+                        img_gary_sun2 = recipess.reccover
+        else:
+            img_gary_sun2 = "blank.jpg"
+    for historys in history:
+        if historys.sun3:
+            a=historys.sun3.split(']')[1]
+            b=historys.sun3.split(']')[0]
+            c=b.split('[')[1]
+            print(c)
+            print(a)
+            if c == "店家":
+                storemenus = Storemenu.objects.all()
+                for storemenuss in storemenus:
+                    if a == storemenuss.cname:
+                        print(storemenuss.cimg)
+                        img_gary_sun3 = storemenuss.cimg
+            elif c == "食譜":
+                recipes = Recipe.objects.all()
+                for recipess in recipes:
+                    if a == recipess.recname:
+                        print(recipess.reccover)
+                        img_gary_sun3 = recipess.reccover
+        else:
+            img_gary_sun3 = "blank.jpg"
+
+
+
+    return render(request, 'gary/historydata.html',locals())
